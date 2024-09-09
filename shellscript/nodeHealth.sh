e9ea1326c38d91cedc8a8811c11618980812e196
sayyamyadav <sayyamyadav01@gmail.com>
2024-09-09 06:25:25 -0700

    pushing latest code adding more about how to write a shell script with best practice commonds

--- shellscript/nodeHealth.sh
+++ shellscript/nodeHealth.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#=====================================good practice while writng any shell script commond=============================
+#=====================================good practice while writng any shell script commond============================
+#i
 ## #################################
 #Authore: Sayyam Yadav
 #Date: 9/92024
@@ -10,6 +11,16 @@
 #Version V1
 
 set -x # debug mode
+set -e # error if the script have any error like we have in line 10L if we uncomment that line and comment set -e then script will still execute but if we uncomment s
+set -o pipefail
+#
+# set -e and then try to excute  line 10 then script will not excute it will stop and show error that this line have issue
+
+# drawback if there is a pipe  in script then although we have set -e commond script will still execute for exampe commomn pipefail and add pipe 
+#
+#
+#so that's why here comes set -o commond if we use that and then try to run script line 10L then it will stop excute and show error if we run the script s
+#bjbvjbjvb cjbvjbcjvbjcbv jbvjcbvjc line 10l | echo "sxb" 
 
 df -h
 
@@ -17,3 +28,5 @@ free -g
 
 
 nproc
+
+ps -ef | grep bin | awk -F " " '{print$2}'
