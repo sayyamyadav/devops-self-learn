f97a516786e524a2d961f76fd0c8d542771e1842
sayyamyadav <sayyamyadav01@gmail.com>
2024-09-09 02:49:14 -0700

    commond for checking memory ,disk,cpu usage

---
+++ shellscript/nodeHealth.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#=====================================good practice while writng any shell script commond=============================
+## #################################
+#Authore: Sayyam Yadav
+#Date: 9/92024
+#
+#this script output the node Health
+#
+#
+#Version V1
+
+set -x # debug mode
+
+df -h
+
+free -g
+
+
+nproc
---
+++ shellscript/sm.sh
@@ -0,0 +1,1 @@
+wnsidcnjcbjbvjbvjbjvj:idcnjcbjbvjbvjbjvj:
