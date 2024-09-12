dc8d4d67af84860ec579961f1c244e9c64d5c5db
sayyamyadav <sayyamyadav01@gmail.com>
2024-09-12 00:03:53 -0700

    work on practice task for shell script

---
+++ shellscript/ifelse.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Authore: Sayyam
+#Date: 9/9/2024
+# if else loop in shell script excuation
+#
+#
+a=4
+b=4
+
+if [$a == $b]
+then
+	echo "print a and b are equal"
+else
+	echo "a and b are not equal"
+fi
+
---
+++ shellscript/task1.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#Authore:Sayyam Yadav
+#Date: 11/9/2024
+#
+#
+# task to perform 
+#
+#
+# create directory my project inside that create subfolder soure,bin,doc
+# print the current directoryy you are at
+# move inside source folder
+# and then print current  directory yu are at
+
+
+# create a myProjectFold 
+mkdir myProject
+cd myProject
+
+# create subfolder src,bin and doc
+#
+mkdir src bin doc
+
+#print the currrent dirc you are at#
+
+pwd
+# mv to src folder
+cd src
+# print the current directory yo are at
+#
+pwd
